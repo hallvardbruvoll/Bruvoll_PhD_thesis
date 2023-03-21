008970ed5f219af9a5509e9c210b36c3a71ce7ed
hallvardbruvoll <hallvard.bruvoll@iakh.uio.no>
2023-03-21 17:43:05 +0100

    Setup and headings Part I

---
+++ Special_characters_encoding.R
@@ -0,0 +1,16 @@
+Lognormal:
+mean/mu = \u03bc
+sd/sigma = \u03c3
+
+Exponential:
+rate/lambda = \u03bb
+Stretched exponential/Weibull:
+scale/lambda = \u03bb
+shape = k
+
+Power-law:
+scaling parameter/alpha = \u03b1
+
+Slovak:
+slovak_characters <- read.csv2("Data/site_names_unicode.csv",
+                               encoding = "UTF-8")
