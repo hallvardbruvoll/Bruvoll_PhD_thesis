64ceecf2eec8b12247317ad693ae5f1c6356fa08
hallvardbruvoll <hallvard.bruvoll@iakh.uio.no>
2023-08-06 19:03:20 +0200

    More test plots for chapter 8

--- Analysis/08-images-methods.R
+++ Analysis/08-images-methods.R
@@ -24,27 +24,146 @@ extract.plot.size <- function(plot.input) {
 
 # Test plots --------------------------------------------------------------
 
-# Variable element count (N), constant density, size distribution and layout
 
-iterations <- 15
+iterations <- 20
 N_plots <- list()
+
+# Variable element count (N), constant density, size distribution and layout
+  # First by varying the size of the image
+  # i is number of boxes in a row, starting with 2
 for (i in 2:(iterations+1)) {
   one_plot <- tibble(x = rep(1:i, i),
                      y = rep(1:i, each = i))
   plot_name <- paste0("N_plot_", i-1)
   N_plots[[plot_name]] <- ggplot(one_plot)+
     aes(x, y, width = 0.5, height = 0.5)+
-    geom_tile(fill = "black", linetype = 0)+ # these aesthetics are needed
+    geom_tile(fill = "black", colour = NA)+ # fill must be explicitly set
                                             # for the fract2D to work!
     theme_void()+
     scale_x_continuous(limits = c(0.75, i+0.75), expand = c(0,0))+
     scale_y_continuous(limits = c(0.75, i+0.75), expand = c(0,0))+
-    theme(plot.background = element_rect(fill = "white", color = NA))+
+    theme(plot.background = element_rect(fill = "white", colour = NA))+
     coord_fixed()
 }
-N_plots$N_plot_10+theme(panel.border = element_rect(colour = "black",
-                                                   size = 1, fill = NA))
-N_plots[[2]]
+
+  # Second by varying the size of houses (single image size)
+for (i in 2:(iterations+1)) {
+  unit_length <- (10/i)/2
+  one_plot <- tibble(x = rep(seq(0, 9.99999, by = 10/i)+(unit_length/2), i),
+                     y = rep(seq(0, 9.99999, by = 10/i)+(unit_length/2), each = i),
+                     height = unit_length, width = unit_length)
+  plot_name <- paste0("N_fix_plot_", i-1)
+  limits <- c(0,10)
+
+  N_plots[[plot_name]] <- ggplot(one_plot)+
+    aes(x, y, width = width, height = height)+
+    geom_tile(fill = "black", colour = NA)+
+    theme_void()+
+    scale_x_continuous(limits = limits, expand = c(0,0))+
+    scale_y_continuous(limits = limits, expand = c(0,0))+
+    theme(plot.background = element_rect(fill = "white", colour = NA))+
+    coord_fixed()
+}
+
+# Variable density, constant N, size distribution and layout
+  # Keep sequence to steps of 0.05 (pixel size or 0.5m)
+unit_length <- seq(0.05, 1, length.out = iterations)
+limits <- c(0.5,9.5)
+for (i in 1:iterations) {
+  one_plot <- tibble(x = rep(1:9, 9),
+                     y = rep(1:9, each = 9),
+                     height = unit_length[i],
+                     width = unit_length[i])
+  plot_name <- paste0("dens_plot_", i)
+
+  N_plots[[plot_name]] <- ggplot(one_plot)+
+    aes(x, y, width = width, height = height)+
+    geom_tile(fill = "black", colour = NA)+
+    theme_void()+
+    scale_x_continuous(limits = limits, expand = c(0,0))+
+    scale_y_continuous(limits = limits, expand = c(0,0))+
+    theme(plot.background = element_rect(fill = "white", colour = NA))+
+    coord_fixed()
+}
+
+# Variable size distribution, constant N, density and layout
+  # Keep in mind to round to nearest 0.05 for image resolution
+set.seed(100)
+sigma <- seq(0.1, 1.1, length.out = 20)
+limits <- c(0.5,9.5)
+for (i in 1:iterations) {
+  sizes <- rlnorm(n = 81, meanlog = 3.5, sdlog = sigma[i])
+  # normalise sum of sizes to 1
+  sizes <- sizes/sum(sizes)
+  # multiply to wanted total area
+    # and keep to nearest 0.05 for image resolution
+  sizes <- round(sqrt(sizes*(9.5^2/6))*2, 1)/2
+  one_plot <- tibble(x = rep(1:9, 9),
+                     y = rep(1:9, each = 9),
+                     height = sizes,
+                     width = sizes)
+  plot_name  <- paste0("distr_plot_", i)
+
+  N_plots[[plot_name]] <- ggplot(one_plot)+
+    aes(x, y, width = width, height = height)+
+    geom_tile(fill = "black", colour = NA)+
+    theme_void()+
+    scale_x_continuous(limits = limits, expand = c(0,0))+
+    scale_y_continuous(limits = limits, expand = c(0,0))+
+    theme(plot.background = element_rect(fill = "white", colour = NA))+
+    coord_fixed()
+}
+
+# Variable layout, constant N, density and size distribution
+limits <- c(0.5, 8.5)
+one_plot <- tibble(x = rep(1:8, 8),
+                   y = rep(1:8, each = 8),
+                   height = 0.5, width = 0.5)
+ggplot(one_plot)+
+  aes(x, y, width = width, height = height)+
+  geom_tile(fill = "black", colour = NA)+
+  scale_x_continuous(limits = limits, expand = c(0,0))+
+  scale_y_continuous(limits = limits, expand = c(0,0))+
+  theme_bw()+
+  coord_fixed()
+
+N_plots$distr_plot_20+theme_bw()
+# DELETE
+
+# Sierpinski carpet -------------------------------------------------------
+
+init1 <- 1/2
+init2 <- 1/3
+number_of_iterations <- 2
+iter <- tibble(n = 1:number_of_iterations,
+               gen = 3^n)
+
+
+Sierp <- tibble(x = init1, y = x, w = init2, h = w)
+
+for (j in 1:nrow(iter)) {
+  new_x <- seq(init1/iter$gen[j], 1, by = init1/iter$gen[j]*2)
+  for (i in 1:length(new_x)) {
+    one_row <- tibble(x = new_x, y = new_x[i], w = init2/iter$gen[j], h = w)
+    Sierp <- bind_rows(Sierp, one_row)
+  }
+}
+
+
+ggplot(Sierp)+
+  aes(x = x, y = y, width = w, height = h)+
+  geom_tile(aes(width = 1), fill = "white")+
+  geom_tile(aes(height = 1), fill = "white")+
+  scale_x_continuous(limits = c(0,1), expand=c(0,0)) +
+  scale_y_continuous(limits = c(0,1), expand=c(0,0)) +
+  theme_void()+
+  coord_fixed()+
+  theme(plot.background = element_rect(fill = "black", color = NA))
+
+
+
+
+# STOP DELETE
 
   #save plots
 for (i in 1:length(N_plots)) {
@@ -64,16 +183,6 @@ for (i in 1:length(N_plots)) {
          units = "px")
 }
 
-  #convert jpeg to greyscale
-filenames <- list.files(path = "Data/Frac_test/")
-length(filenames)
-for (i in 1:length(filenames)) {
-  path <- paste0("Data/Frac_test/", filenames[i])
-  one_image <- load.image(path)
-  one_image <- grayscale(one_image, method = "Luma")
-  save.image(one_image, file = path, quality = 1)
-}
-
 
 test <- frac.lac(frac_path = "Data/Frac_test", lac_path = "Data/Lac_test")
 test <- test$D_L_plot %>%
@@ -84,3 +193,13 @@ ggplot(test)+
   ggrepel::geom_text_repel()
 
 test
+
+#convert jpeg to greyscale when all images are made
+filenames <- list.files(path = "Data/Frac_test/")
+length(filenames)
+for (i in 1:length(filenames)) {
+  path <- paste0("Data/Frac_test/", filenames[i])
+  one_image <- load.image(path)
+  one_image <- grayscale(one_image, method = "Luma")
+  save.image(one_image, file = path, quality = 1)
+}
