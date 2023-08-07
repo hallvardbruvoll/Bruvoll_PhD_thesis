4ce300d7325ce3b31adc00c9ce7301a78d866546
hallvardbruvoll <hallvard.bruvoll@iakh.uio.no>
2023-08-07 21:59:16 +0200

    Added figures to Chapter 8, tests for effects on D and L from other variables

--- Analysis/08-images-methods.R
+++ Analysis/08-images-methods.R
@@ -2,6 +2,7 @@
 
 # Load functions and libraries
 source("Analysis/Image-functions.R")
+library(ggimage) #for geom_image
 
 # Test plots --------------------------------------------------------------
 
@@ -209,3 +210,255 @@ save(N_plots, file = "Data/N_plots.RData")
 #   ggrepel::geom_text_repel()
 #
 # test
+
+# Plots for each series ---------------------------------------------------
+
+  # Tidy and arrange by groups
+D_L_tests$D_L_plot
+D_L_test_plots <- D_L_tests$D_L_plot
+
+D_L_test_plots <- D_L_test_plots %>%
+  mutate(Series = str_remove_all(id, pattern = "[:digit:]"),
+         Series = str_replace(Series, pattern = "N_fix_plot_", "N objects"),
+         Series = str_replace(Series, "N_plot_", "Image size"),
+         Series = str_replace(Series, "clust_", "Clustering"),
+         Series = str_replace(Series, "dens_plot_", "Density"),
+         Series = str_replace(Series, "distr_plot_", "Size distr."),
+         Series = str_replace(Series, "noise_", "Noise"),
+         Series = as.factor(Series),
+         filename = str_replace(filename, ".tiff", ".jpg"),
+         path = str_c("Data/Frac_test/", filename),
+         Iter = as.numeric(str_remove_all(
+           id, pattern = "[[:punct:][:alpha:]]"))) %>%
+  arrange(Series, Iter)
+
+levels(D_L_test_plots$Series)
+
+# Variable N
+N1 <- ggplot(filter(D_L_test_plots, Series == "N objects"))+
+  aes(D, L_mean, label = Iter)+
+  geom_point()+
+  geom_path()+
+  ggrepel::geom_text_repel()+
+  theme_bw()
+
+N2 <- ggplot(filter(D_L_test_plots, Series == "N objects"))+
+  aes(D, L, label = Iter)+
+  geom_point()+
+  geom_path()+
+  ggrepel::geom_text_repel()+
+  theme_bw()
+
+fig08_N <- plot_grid(N1, N2, nrow = 1, labels = "auto")
+
+  # N images
+N_im_data <- filter(D_L_test_plots, Series == "N objects"
+                    & Iter %in% c(1,4,8,15,20))
+fig08_N_im <- ggplot(N_im_data)+
+  aes(D, L_mean, label = Iter, image = path)+
+  geom_image(size = 0.2)+
+  geom_text(data = slice(N_im_data, c(1,2,4)), nudge_x = 0.035)+
+  geom_text(data = slice(N_im_data, c(3,5)), nudge_x = -0.035)+
+  scale_x_continuous(expand = c(0.1, 0.1))+
+  scale_y_continuous(expand = c(0.2, 0.2))+
+  theme_bw()+
+  theme(panel.background = element_rect(fill = "lightgrey"),
+        panel.grid = element_blank())
+
+
+# Variable image size
+im_size1 <- ggplot(filter(D_L_test_plots, Series == "Image size"))+
+  aes(D, L_mean, label = Iter)+
+  geom_point()+
+  geom_path()+
+  ggrepel::geom_text_repel()+
+  theme_bw()
+
+im_size2 <- ggplot(filter(D_L_test_plots, Series == "Image size"))+
+  aes(D, L, label = Iter)+
+  geom_point()+
+  geom_path()+
+  ggrepel::geom_text_repel()+
+  theme_bw()
+
+fig08_im_size <- plot_grid(im_size1, im_size2, nrow = 1, labels = "auto")
+
+  # Image size im
+size_im_data <- filter(D_L_test_plots, Series == "Image size" &
+                         Iter %in% c(1,2,3,6,20))
+fig08_size_im <- ggplot(size_im_data)+
+  aes(D, L_mean, label = Iter, image = path)+
+  geom_text(data = slice(size_im_data, 1:3), nudge_x = -0.06)+
+  geom_text(data = slice(size_im_data, 4), nudge_x = -0.1)+
+  geom_text(data = slice(size_im_data, 5), nudge_x = -0.3, nudge_y = -0.1)+
+  geom_image(aes(size = I(Iter/40)))+
+  scale_x_continuous(expand = c(0.1, 0.2))+
+  scale_y_continuous(expand = c(0.6, 0))+
+  theme_bw()+
+  theme(panel.background = element_rect(fill = "lightgrey"),
+        panel.grid = element_blank())
+
+# Variable image density
+dens1 <- ggplot(filter(D_L_test_plots, Series == "Density"))+
+  aes(D, L_mean, label = Iter)+
+  geom_point()+
+  geom_path()+
+  ggrepel::geom_text_repel()+
+  theme_bw()
+
+dens2 <- ggplot(filter(D_L_test_plots, Series == "Density"))+
+  aes(D, L, label = Iter)+
+  geom_point()+
+  geom_path()+
+  ggrepel::geom_text_repel()+
+  theme_bw()
+
+fig08_dens <- plot_grid(dens1, dens2, nrow = 1, labels = "auto")
+
+  # Density images
+fig08_dens_im <- ggplot(filter(D_L_test_plots, Series == "Density"
+                                & Iter %in% c(2,4,6,10,16,20)))+
+  aes(D, L_mean, label = Iter, image = path)+
+  geom_image(size = 0.2)+
+  geom_text(nudge_y = 4.5)+
+  scale_x_continuous(expand = c(0.1, 0.1))+
+  scale_y_continuous(expand = c(0.2, 0))+
+  theme_bw()+
+  theme(panel.background = element_rect(fill = "lightgrey"),
+        panel.grid = element_blank())
+
+# Variable size distribution
+dist1 <- ggplot(filter(D_L_test_plots, Series == "Size distr."))+
+  aes(D, L_mean, label = Iter)+
+  geom_point()+
+  geom_path()+
+  ggrepel::geom_text_repel()+
+  theme_bw()
+
+dist2 <- ggplot(filter(D_L_test_plots, Series == "Size distr."))+
+  aes(D, L, label = Iter)+
+  geom_point()+
+  geom_path()+
+  ggrepel::geom_text_repel()+
+  theme_bw()
+
+fig08_distr <- plot_grid(dist1, dist2, nrow = 1, labels = "auto")
+
+  # Size distribution images
+fig08_distr_im <- ggplot(filter(D_L_test_plots, Series == "Size distr."
+                                     & Iter %in% c(1,4,8,14,18,19)))+
+  aes(D, L_mean, label = Iter, image = path)+
+  geom_image(size = 0.2)+
+  geom_text(nudge_y = 0.1)+
+  scale_x_continuous(expand = c(0.005, 0.005))+
+  scale_y_continuous(expand = c(0.05, 0.05))+
+  theme_bw()+
+  theme(panel.background = element_rect(fill = "lightgrey"),
+        panel.grid = element_blank())
+
+
+# Variable clustering
+cluster1 <- ggplot(filter(D_L_test_plots, Series == "Clustering"))+
+  aes(D, L_mean, label = Iter)+
+  geom_point()+
+  geom_path()+
+  ggrepel::geom_text_repel()+
+  theme_bw()
+
+cluster2 <- ggplot(filter(D_L_test_plots, Series == "Clustering"))+
+  aes(D, L, label = Iter)+
+  geom_point()+
+  geom_path()+
+  ggrepel::geom_text_repel()+
+  theme_bw()
+
+fig08_clustering <- plot_grid(cluster1, cluster2, nrow = 1, labels = "auto")
+
+  # Clustering images
+fig08_clustering_im <- ggplot(filter(D_L_test_plots, Series == "Clustering"
+                                & Iter %in% c(1,7,13,18,20)))+
+  aes(D, L_mean, label = Iter, image = path)+
+  geom_image(size = 0.2)+
+  geom_text(nudge_y = 0.075)+
+  scale_x_continuous(expand = c(0.005, 0.005))+
+  scale_y_continuous(expand = c(0.05, 0.05))+
+  theme_bw()+
+  theme(panel.background = element_rect(fill = "lightgrey"),
+        panel.grid = element_blank())
+
+
+# Variable noise
+noise1 <- ggplot(filter(D_L_test_plots, Series == "Noise"))+
+  aes(D, L_mean, label = Iter)+
+  geom_point()+
+  geom_path()+
+  ggrepel::geom_text_repel()+
+  theme_bw()
+
+noise2 <- ggplot(filter(D_L_test_plots, Series == "Noise"))+
+  aes(D, L, label = Iter)+
+  geom_point()+
+  geom_path()+
+  ggrepel::geom_text_repel()+
+  theme_bw()
+
+fig08_noise <- plot_grid(noise1, noise2, nrow = 1, labels = "auto")
+
+  # Noise images
+noise_im_data <- filter(D_L_test_plots, Series == "Noise" &
+                          Iter %in% c(1,11,15,20))
+fig08_noise_im <- ggplot(noise_im_data)+
+  aes(D, L_mean, label = Iter, image = path)+
+  geom_image(size = 0.2)+
+  geom_text(data = slice(noise_im_data, c(1,3)), nudge_x = -0.0013)+
+  geom_text(data = slice(noise_im_data, c(2,4)), nudge_x = 0.0013)+
+  scale_x_continuous(expand = c(0.002, 0.002))+
+  scale_y_continuous(expand = c(0.01, 0.01))+
+  theme_bw()+
+  theme(panel.background = element_rect(fill = "lightgrey"),
+        panel.grid = element_blank())
+
+# Maybe not use figure of all images? Or add it in the end of the chapter?
+all1 <- ggplot(D_L_test_plots)+
+  aes(D, L_mean, size = Iter, colour = Series)+
+  geom_point(alpha = 0.6)+
+  scale_size_area(max_size = 4)+
+  theme_bw()+
+  theme(legend.position = "bottom")
+
+all2 <- ggplot(D_L_test_plots)+
+  aes(D, L, size = Iter, colour = Series)+
+  geom_point(alpha = 0.6)+
+  scale_size_area(max_size = 4)+
+  theme_bw()+
+  theme(legend.position = "none")
+
+all_legend <- get_legend(all1)
+fig08_all <- plot_grid(plot_grid(all1+theme(legend.position = "none"),
+                                 all2, nrow = 1, labels = "auto"),
+                       all_legend, ncol = 1, rel_heights = c(1, 0.1))
+# Note to self: From this it seems density is the only factor that
+# has significant impact on both D and L
+# Image size (N_plot) has important impact on D
+# The other factors (clustering, noise and size distribution) have much more
+# limited impact.
+# However, the applied range of each of these factors is arbitrary and
+# not straightforward to compare between them.
+
+# Save plots
+save(fig08_all, file = "Results/fig08_all.RData")
+save(fig08_noise, file = "Results/fig08_noise.RData")
+save(fig08_clustering, file = "Results/fig08_clustering.RData")
+save(fig08_distr, file = "Results/fig08_distr.RData")
+save(fig08_dens, file = "Results/fig08_dens.RData")
+save(fig08_im_size, file = "Results/fig08_im_size.RData")
+save(fig08_N, file = "Results/fig08_N.RData")
+
+ggsave("Results/fig08_noise_im.pdf", plot = fig08_noise_im)
+ggsave("Results/fig08_clustering_im.pdf", plot = fig08_clustering_im)
+ggsave("Results/fig08_distr_im.pdf", plot = fig08_distr_im)
+ggsave("Results/fig08_dens_im.pdf", plot = fig08_dens_im)
+ggsave("Results/fig08_size_im.pdf", plot = fig08_size_im)
+ggsave("Results/fig08_N_im.pdf", plot = fig08_N_im)
+
+# END CHAPTER
